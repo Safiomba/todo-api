080f6f2062a289e52a44488b99a88e72311b92e7
Ansoumana CISSE <ansoumana.cisse@capgemini.com>
2016-06-20 23:25:58 +0200

    add /

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 var express = require('express');
+var _=require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req,resp){
-	resp.send('Todo API Root !');
+var todos =[{
+	id:1,
+	description:'Take launch with mom',
+	completed:false
+
+},{
+	id:2,
+	description:'Learn node js ',
+	completed:false
+}
+]
+
+app.get('/', function(req,res){
+	res.send('Todo API Root !');
 });
-//app.use(express.static(__dirname + '/public'));
+
+app.get('/todos', function(req,res){
+	res.json(todos);
+
+});
+
+app.get('/todos/:id', function(req, res){
+	//res.send('Asking for todo with id :' + req.params.id);
+	var todo = _.find(todos, function(item){
+		return item.id === parseInt(req.params.id,10);
+	})
+	if(todo){
+		res.json(todo);
+	}else{
+		res.status(404).send();
+	}
+
+		
+});
 
 app.listen(PORT , function(){
 	console.log('Express server started on port :' + PORT);

8ca35eb3e383036e14ee4fab4ffb6e34cc0355cc
Ansoumana CISSE <ansoumana.cisse@capgemini.com>
2016-06-24 01:05:48 +0200

    Fixe sequelize POST /todo/:id

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, constants.description, constants.completed);
 	db.todo.create(body).then(function(todo) {
-
+		res.json(todo.toJSON());
 	}, function(e) {
 		res.status(400).json(e);
 
